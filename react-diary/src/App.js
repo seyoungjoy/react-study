7e32e090dcc0e07a2c12524c515ef2a688fb7ba1
seyoungjoy <diddpwl80@gmail.com>
2022-08-19 09:53:11 +0900

    :sparkles: Add: editor form

--- react-diary/src/App.js
+++ react-diary/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import DiaryEditor from './DiaryEditor'
 function App() {
   return (
     <div className="App">
-
+      <h1>오늘의 일기</h1>
+      <DiaryEditor/>
     </div>
   );
 }
---
+++ react-diary/src/DiaryEditor.js
@@ -0,0 +1,63 @@
+import styled from '@emotion/styled';
+import {useState} from "react";
+
+const DiaryEditor = () => {
+    const [state, setState] = useState({
+        title: '',
+        content: '',
+        score:1,
+    });
+    const onChangeHandler = (e) => {
+        setState({
+            ...state,
+            [e.target.id] : e.target.value
+        });
+    }
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        alert('저장 완료!')
+        console.log(state);
+    }
+
+    return(
+        <div>
+            <form>
+                <dl>
+                    <dt>
+                        <label htmlFor="title">제목</label>
+                    </dt>
+                    <dd>
+                        <input type="text" id={"title"} value={state.title} onChange={onChangeHandler}/>
+                        {/*<div>{state.title}</div>*/}
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>
+                        <label htmlFor="content">내용</label>
+                    </dt>
+                    <dd>
+                        <textarea name="content" id="content" cols="30" rows="10" value={state.content} onChange={onChangeHandler}></textarea>
+                        {/*<div>{state.content}</div>*/}
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>
+                        <label htmlFor="score">오늘의 감정점수 : {state.score}</label>
+                    </dt>
+                    <dd>
+                        <select name="score" id="score" onChange={onChangeHandler}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </dd>
+                </dl>
+                <button onClick={onSubmitHandler}>일기 저장하기</button>
+            </form>
+        </div>
+    )
+}
+
+export default DiaryEditor;
